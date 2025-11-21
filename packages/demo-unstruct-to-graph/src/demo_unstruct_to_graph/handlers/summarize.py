bdefee7b93144d019e81e0c41bdc2becc7ee5fed
Martin Schaer <martin.schaer@surrealdb.com>
2025-11-21 16:43:43 +0000

    fix summary record ids

--- packages/demo-unstruct-to-graph/src/demo_unstruct_to_graph/handlers/summarize.py
+++ packages/demo-unstruct-to-graph/src/demo_unstruct_to_graph/handlers/summarize.py
@@ -9,7 +9,9 @@ from kaig.db import DB
 logger = logging.getLogger(__name__)
 
 
-def summarize_handler(db: DB, chunk_rec_id: RecordID) -> None:
+def summarize_handler(
+    db: DB, chunk_rec_id: RecordID, force: bool = False
+) -> None:
     logger.info("Starting inference...")
 
     if not db.llm:
@@ -22,14 +24,22 @@ def summarize_handler(db: DB, chunk_rec_id: RecordID) -> None:
     if chunk is None:
         raise ValueError(f"Chunk not found {chunk_rec_id}")
 
-    logger.info(f"Summarizing chunk: {chunk_rec_id}")
-    summary = db.llm.summarize(chunk.content)
-    summary_id = RecordID(Tables.summary.value, summary)
-    _ = db.query_one(
-        "UPSERT ONLY $record",
-        {"record": summary_id},
-        dict[str, JsonValue],
+    summary_id = RecordID(
+        Tables.summary.value,
+        chunk.id.id,  # pyright: ignore[reportAny]
     )
+
+    if db.exists(summary_id) and not force:
+        logger.info(f"Summary already exists {summary_id}")
+    else:
+        logger.info(f"Summarizing chunk: {chunk_rec_id}")
+        summary = db.llm.summarize(chunk.content)
+        _ = db.query_one(
+            "UPSERT ONLY $record SET content = $content",
+            {"record": summary_id, "content": summary},
+            dict[str, JsonValue],
+        )
+
     db.relate(
         chunk.id,
         EdgeTypes.SUMMARIZED_BY.value.name,
