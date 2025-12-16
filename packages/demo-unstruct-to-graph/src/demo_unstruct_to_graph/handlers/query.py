66fac325cc79445e2cb66ad2a346add2634ef743
Martin Schaer <martin.schaer@surrealdb.com>
2025-12-16 16:21:23 +0000

    working

--- packages/demo-unstruct-to-graph/src/demo_unstruct_to_graph/handlers/query.py
+++ packages/demo-unstruct-to-graph/src/demo_unstruct_to_graph/handlers/query.py
@@ -1,14 +1,17 @@
 import logging
 import sys
 from dataclasses import dataclass
+from pathlib import Path
+from typing import cast
 
 import logfire
 from openai import AsyncOpenAI
 from pydantic_ai import Agent, RunContext
+from surrealdb import Value
 
 from demo_unstruct_to_graph.db import init_db
-from demo_unstruct_to_graph.definitions import Chunk
 from kaig.db import DB
+from kaig.db.utils import query
 
 logger = logging.getLogger(__name__)
 stdout = logging.StreamHandler(stream=sys.stdout)
@@ -17,10 +20,31 @@ logger.setLevel(logging.DEBUG)
 logger.addHandler(stdout)
 
 
+surql_path = Path(__file__).parent.parent / "surql" / "search.surql"
+with open(surql_path, "r") as file:
+    q = file.read()
+
+
 @dataclass
 class Deps:
     db: DB
     openai: AsyncOpenAI
+
+
+# @dataclass
+# class ResultChunk:
+#     chunk: str
+#     score: float
+#     chunk_index: int
+#     content: str
+
+
+@dataclass
+class SearchResult:
+    doc: str
+    best_chunk_score: float
+    # chunks: list[ResultChunk]
+    chunks: list[dict[str, str | float | int]]
 
 
 MODEL = "openai:gpt-5-mini-2025-08-07"
@@ -35,19 +59,27 @@ async def retrieve(context: RunContext[Deps], search_query: str) -> str:
         context: The call context.
         search_query: The search query.
     """
+    db = context.deps.db
+
     with logfire.span(
-        "vector search for {search_query=}", search_query=search_query
+        "vector+graph search for {search_query=}", search_query=search_query
     ):
-        chunks, _time = context.deps.db.vector_search_from_text(
-            Chunk, search_query, table="chunk", k=5
+        if db.embedder is None:
+            raise ValueError("Embedder is not configured")
+
+        embedding = db.embedder.embed(search_query)
+        results = query(
+            db.sync_conn,
+            q,
+            {"embedding": cast(Value, embedding), "threshold": 0.2},
+            SearchResult,
         )
-        # TODO: get documents related to chunks
 
     results = "\n\n".join(
-        f"# Chunk {chunk.id} ({score:.2f})\n{chunk.content}\n"
-        for chunk, score in chunks
+        f"# Document {x.doc} ({x.best_chunk_score:.2f})\n{'\n\n'.join(str(y['content']) for y in x.chunks)}\n"
+        for x in results
     )
-    logger.debug("Retrieved chunks: %s", results)
+    logger.debug("Retrieved data: %s", results)
     return results
 
 
@@ -69,14 +101,13 @@ if __name__ == "__main__":
 
     _ = logfire.configure(send_to_logfire="if-token-present")
     logfire.instrument_pydantic_ai()
-    # TODO: uncomment this
-    # logfire.instrument_surrealdb()
+    logfire.instrument_surrealdb()
 
-    query = sys.argv[1]
-    print(f"query: {query}")
+    question = sys.argv[1]
+    print(f"question: {question}")
 
     db = init_db(init_llm=False, init_indexes=False)
 
-    res = asyncio.run(query_handler(db, query))
+    res = asyncio.run(query_handler(db, question))
 
     print(f"result: {res}")
